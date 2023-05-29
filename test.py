50d47898ad6ab4336b01f69ee45eb555e2fd90a0
JongJin Lee <kaejong2@gmail.com>
2023-05-29 18:35:37 +0900

    Doc. Issue delete

--- test.py
+++
@@ -1,7 +0,0 @@
-# Settings
-from pathlib import Path
-import pkg_resources as pkg
-FILE = Path(__file__).resolve()
-PARENT = FILE.parent  # root directory
-README = (PARENT / 'README.md').read_text(encoding='utf-8')
-REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
